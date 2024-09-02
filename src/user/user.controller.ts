aa48a52ca07786aeed8012f526698d11f03890d3
xzw <1272117264@qq.com>
2024-09-02 22:54:23 +0800

    feat: 添加修改密码功能

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,44 @@
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Post('/update')
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Body() data: { username: string; password: string; id: number },
+  ) {
+    const { username, password, id } = data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    if (/^(?=.*\d)(?=.*[A-z])[\da-zA-Z]{8}$/.test(password) === false) {
+      throw new BadRequestException('密码格式错误');
+    }
+
+    if (username && /^(?=.*[a-zA-Z]).{4,16}$/.test(username) === false) {
+      throw new BadRequestException('用户名格式错误');
+    }
+
+    const existUser = await this.userService.findOneByUsername(username);
+    if (existUser && existUser.id !== id) {
+      throw new BadRequestException('用户名已使用');
+    }
+
+    await this.userService.update(id, {
+      username: username === '' ? undefined : username,
+      password: hashedPassword,
+    });
+
+    return { message: '修改成功' };
+  }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [PrismaModule],
-  providers: [UserService],
+  providers: [UserService, AuthService, JwtService],
   exports: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
